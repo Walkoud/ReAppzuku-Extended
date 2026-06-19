a4d0f081a8bfdc54ba7877d582fdefc83aca6e93
gree1d <161749879+gree1d@users.noreply.github.com>
2026-06-20 01:16:38 +0400

    Add files via upload

---
+++ app/src/main/java/com/gree1d/reappzuku/utils/KillShortcutActivity.java
@@ -0,0 +1,17 @@
+package com.gree1d.reappzuku.utils;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class KillShortcutActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent service = new Intent(this, ShappkyService.class);
+        service.setAction("WIDGET_KILL");
+        startService(service);
+        finish();
+    }
+}
