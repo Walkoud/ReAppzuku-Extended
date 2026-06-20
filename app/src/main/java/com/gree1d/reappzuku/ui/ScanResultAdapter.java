c2df0b2fed331f79122bd7894cd0970a2f62ebce
gree1d <161749879+gree1d@users.noreply.github.com>
2026-06-20 11:16:01 +0400

    Update ScanResultAdapter.java

--- app/src/main/java/com/gree1d/reappzuku/ui/ScanResultAdapter.java
+++ app/src/main/java/com/gree1d/reappzuku/ui/ScanResultAdapter.java
@@ -19,6 +19,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.gree1d.reappzuku.manager.ScanSystem;
+import com.gree1d.reappzuku.R;
 
 public class ScanResultAdapter extends RecyclerView.Adapter<ScanResultAdapter.ViewHolder> {
 
