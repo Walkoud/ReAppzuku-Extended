727743c4dad5bfc862f26d35fe5d5a3988533995
gree1d <161749879+gree1d@users.noreply.github.com>
2026-06-20 10:11:53 +0400

    Update KillShortcutActivity.java

--- app/src/main/java/com/gree1d/reappzuku/utils/KillShortcutActivity.java
+++ app/src/main/java/com/gree1d/reappzuku/utils/KillShortcutActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.gree1d.reappzuku.service.ShappkyService;
+
 public class KillShortcutActivity extends Activity {
 
     @Override
